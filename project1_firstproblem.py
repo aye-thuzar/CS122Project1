af811aad319313eb517e57a590269ef42208d6d8
Aye Thuzar <ayethuzar@gmail.com>
2024-10-07 15:01:41 -0700

    Add files via upload

---
+++ project1_firstproblem.py
@@ -0,0 +1,13 @@
+percentage = 92.5
+
+
+if percentage >= 92:
+    print("A")
+if percentage >= 82:
+    print("B")
+if percentage >= 68:
+    print("C")
+if percentage >= 62:
+    print("D")
+else:
+    print("F")
